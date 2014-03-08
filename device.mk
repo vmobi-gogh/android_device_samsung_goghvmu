8d897d2d5fa22b1cbb709b27b021f8cc6af5ed48
John Kirk <jkirk777@gmail.com>
2014-03-07 18:24:29 -0700

    Fix path to vendor:: Abandon old makefile

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/gogh/gogh-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/goghvmu/goghvmu-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/goghvmu/overlay
